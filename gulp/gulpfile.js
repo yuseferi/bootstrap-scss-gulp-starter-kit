381957b0729f173a778579a535174684ea807dfd
Dana <dana@dmxt.org>
2015-10-31 17:14:25 -0400

    Update

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,28 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var browserSync = require('browser-sync');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var plumber     = require('gulp-plumber');
+var sass        = require('gulp-sass');
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: ".."
+    });
+
+    gulp.watch("../assets/scss/*.scss", ['sass']);
+    gulp.watch("../*/**/*").on('change', browserSync.reload);
+    gulp.watch("../*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src('../assets/scss/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sass())
-    .pipe(gulp.dest('../assets/css'))
-    .pipe(browserSync.stream({match: "*.css"}));
+    return gulp.src("../assets/scss/*.scss")
+        .pipe(plumber())
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sass())
+        .pipe(gulp.dest("../assets/css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('browserSync', function() {
-    browserSync({
-        server: {
-            baseDir: '..'
-        },
-        notify: false
-    })
-})
-
-gulp.task('watch', ['browserSync', 'sass'], function (){
-    gulp.watch('../assets/scss/*.scss', ['sass']); 
-    gulp.watch('../*', browserSync.reload);
-    gulp.watch('../**/*', browserSync.reload);
-});
+gulp.task('default', ['serve']);
